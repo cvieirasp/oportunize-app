2be77e070197496b83a652180a348859e54427c3
cvieirasp <cvieirasp@gmail.com>
2025-02-06 20:33:11 -0300

    fix: corrigido arquivo `resend.ts`

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,8 +1,7 @@
 import { Resend } from "resend"
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export const sendVerificationEmail = async (email: string, token: string) => {
+  const resend = new Resend(process.env.RESEND_API_KEY as string)
   const confirmLink = `${process.env.NEXT_PUBLIC_APP_URL}/auth/verify-email?token=${token}`
 
   await resend.emails.send({
