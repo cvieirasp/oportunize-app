114287c6c61e36d685ecb48034a5a84d408923af
cvieirasp <cvieirasp@gmail.com>
2025-02-06 20:39:47 -0300

    fix: alterada versão stripe

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,7 +1,8 @@
 import { Resend } from "resend"
 
+const resend = new Resend(process.env.RESEND_API_KEY)
+
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const resend = new Resend(process.env.RESEND_API_KEY as string)
   const confirmLink = `${process.env.NEXT_PUBLIC_APP_URL}/auth/verify-email?token=${token}`
 
   await resend.emails.send({
